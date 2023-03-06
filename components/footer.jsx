1013d18c5cb89afac570350f1fdb7d407f26030a
Kai-Itakura <itakai199969@gmail.com>
2023-03-07 02:01:34 +0900

    footerの見た目を整えた

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,14 +1,17 @@
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classes from 'styles/footer.module.scss';
 
 const Footer = () => {
     return (
         <footer>
-            <h2>Contact</h2>
-            <address>
-                <FontAwesomeIcon icon={faEnvelope} size='lg' color="rgb(75, 126, 232)" />
-                <a href="mailto:itakai199969@gmail.com">itakai199969@gmail.com</a>
-            </address>
+            <div className={classes.wrapper}>
+                <h2 className={classes.title}>Contact</h2>
+                <address className={classes.flexContainer}>
+                    <FontAwesomeIcon className={classes.icon} icon={faEnvelope} size='lg' color="rgb(75, 126, 232)" />
+                    <a href="mailto:itakai199969@gmail.com">itakai199969@gmail.com</a>
+                </address>
+            </div>
         </footer>
     );
 }
