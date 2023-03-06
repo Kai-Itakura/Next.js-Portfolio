d6636eeda69c7d8ef913ee0b2c2f5b262302614e
Kai-Itakura <itakai199969@gmail.com>
2023-03-07 01:34:54 +0900

    Heroのtitleを中央揃えにした

--- components/hero.jsx
+++ components/hero.jsx
@@ -3,9 +3,11 @@ import classes from 'styles/hero.module.scss'
 const Hero = ({ title, subtitle = false }) => {
     return (
         <div className={classes.container}>
-            <div className={classes.text}>
-                <h1 className={classes.title}>{title}</h1>
-                {subtitle && <p className={classes.subtitle}>{subtitle}</p>}
+            <div className={classes.flexCenter}>
+                <div>
+                    <h1 className={classes.title}>{title}</h1>
+                    {subtitle && <p className={classes.subtitle}>{subtitle}</p>}
+                </div>
             </div>
         </div>
     );
