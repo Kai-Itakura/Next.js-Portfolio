1b0e97d1b1756c5d6de81f75617f2ff34de54a1a
Kai-Itakura <itakai199969@gmail.com>
2023-03-10 05:36:59 +0900

    一定の高さまでスクロールしたらHeaderの背景色を変更

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,38 @@
 import Nav from "./nav";
 import SnsLinks from "./sns-links";
 import classes from 'styles/header.module.scss';
+import { useEffect, useState } from "react";
 
 const Header = () => {
+    const [scrollY, setScrollY] = useState(0);
+    const [headerColor, setHeaderColor] = useState('transparent');
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.pageYOffset);
+        }
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log(scrollY)
+        if (scrollY > 450) {
+            setHeaderColor('rgb(58, 61, 62, .5)');
+        } else {
+            setHeaderColor('transparent');
+        }
+    }, [scrollY])
+
     return (
         <header>
-            <div className={classes.flexContainer}>
+            <div
+                style={{ backgroundColor: headerColor }}
+                className={classes.flexContainer}
+            >
                 <Nav />
                 <SnsLinks />
             </div>
