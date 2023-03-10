d44a4fe50da50407f4cc6fbd9b440985da48e1e0
Kai-Itakura <itakai199969@gmail.com>
2023-03-11 05:13:40 +0900

    Heroのアニメーションを実装

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,16 +1,43 @@
-import { use, useRef } from 'react';
+import { useEffect } from 'react';
 import classes from 'styles/hero.module.scss'
 
 const Hero = ({ title, subtitle = false, backGround }) => {
-    const refContainer = useRef()
+    useEffect(() => {
+        const bubbleArea = document.querySelector('#bubble-area');
+
+        const createBubble = () => {
+            const bubble = document.createElement('span');
+            bubble.className = `${classes.bubble}`;
+
+            const minSize = 10;
+            const maxSize = 40;
+
+            let bubbleSize = Math.random() * (maxSize - minSize) + minSize;
+
+            bubble.style.width = `${bubbleSize}px`;
+            bubble.style.height = `${bubbleSize}px`;
+            bubble.style.left = `${Math.random() * 100}%`;
+            bubble.style.bottom = `${Math.random() * 100}%`;
+
+            bubbleArea.appendChild(bubble);
+
+            setTimeout(() => {
+                bubble.remove();
+            }, 8000);
+        };
+
+        const intervalId = setInterval(createBubble, 100);
+
+        return () => clearInterval(intervalId);
+    }, []);
 
     return (
-        <div className={`
+        <div
+            id='bubble-area'
+            className={`
         ${classes.container} 
         ${backGround && classes[backGround]}
-        `}
-            ref={refContainer}
-        >
+        `}>
             <div className={classes.flexCenter}>
                 <div>
                     <h1 className={classes.title}>{title}</h1>
