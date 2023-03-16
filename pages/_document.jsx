186ab6e8f2964ae89b20409d75b782b2ebc38a29
Kai-Itakura <itakai199969@gmail.com>
2023-03-16 09:21:19 +0900

    作品ページのタイトルが日本語だった場合のフォントを変更

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,7 @@ export default function Document() {
   return (
     <Html lang={siteLang}>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto:wght@500&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Kaisei+Decol:wght@700&family=Pacifico&family=Roboto:wght@500&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
