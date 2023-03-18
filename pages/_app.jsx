95cb3c8509da6e25b50b30c43f6ed2e572179ad8
Kai-Itakura <itakai199969@gmail.com>
2023-03-19 00:26:06 +0900

    Google Analyticsを設定

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,12 +3,46 @@ import '@/styles/globals.scss'
 import 'modern-css-reset/dist/reset.min.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
+import Script from 'next/script'
+import * as gtag from 'lib/gtag'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 config.autoAddCss = false
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.preview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  },[router.events])
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
