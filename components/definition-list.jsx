93920853ce0be1271b044d8ba104ee87cea6d873
Kai-Itakura <itakai199969@gmail.com>
2023-04-03 23:45:40 +0900

    liにユニークなKeyを割り当てた

--- components/definition-list.jsx
+++ components/definition-list.jsx
@@ -1,36 +1,36 @@
-import { faCircleCheck } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import classes from 'styles/definition-list.module.scss';
-import parse from 'html-react-parser';
+import { faCircleCheck } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classes from 'styles/definition-list.module.scss'
+import parse from 'html-react-parser'
 
 const DefinitionList = ({ title, desc, ja = false }) => {
-    const parseString = () => {
-        if (typeof desc === 'string') {
-            return parse(desc);
-        } else {
-            return desc;
-        }
+  const parseString = () => {
+    if (typeof desc === 'string') {
+      return parse(desc)
+    } else {
+      return desc
     }
-    
-    return (
-        <>
-            <dt className={classes.title}>{title}</dt>
-            <dd className={ja ? classes.jaDesc : classes.desc}>{
-                Array.isArray(desc)
-                    ? (
-                        <ul>
-                            {desc.map((tools) => (
-                                <li className={classes.flexContainer}>
-                                    <FontAwesomeIcon icon={faCircleCheck} />
-                                    <p>{tools}</p>
-                                </li>
-                            ))}
-                        </ul>
-                    ) :
-                    parseString()
-            }</dd>
-        </>
-    );
+  }
+
+  return (
+    <>
+      <dt className={classes.title}>{title}</dt>
+      <dd className={ja ? classes.jaDesc : classes.desc}>
+        {Array.isArray(desc) ? (
+          <ul>
+            {desc.map((tools, index) => (
+              <li className={classes.flexContainer} key={index}>
+                <FontAwesomeIcon icon={faCircleCheck} />
+                <p>{tools}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          parseString()
+        )}
+      </dd>
+    </>
+  )
 }
 
-export default DefinitionList;
+export default DefinitionList
