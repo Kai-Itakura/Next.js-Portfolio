58f81786de9a3162430ff1cb047eab02f2b698e2
Kai-Itakura <itakai199969@gmail.com>
2023-04-04 03:06:17 +0900

    MicroCMSのサービスドメインを環境変数で定義

--- lib/api.jsx
+++ lib/api.jsx
@@ -1,45 +1,45 @@
-import { createClient } from "microcms-js-sdk";
+import { createClient } from 'microcms-js-sdk'
 
 export const client = createClient({
-    serviceDomain: 'kai-itakura',
-    apiKey: process.env.API_KEY,
-});
+  serviceDomain: process.env.SERVICE_DOMAIN,
+  apiKey: process.env.API_KEY
+})
 
 export const getPostBySlug = async (slug) => {
-    try {
-        const post = await client.get({
-            endpoint: 'works',
-            queries: { filters: `slug[equals]${slug}` },
-        });
-        return post.contents[0];
-    } catch (err) {
-        console.log('~~ getPostBySlug ~~');
-        console.log(err);
-    }
+  try {
+    const post = await client.get({
+      endpoint: 'works',
+      queries: { filters: `slug[equals]${slug}` }
+    })
+    return post.contents[0]
+  } catch (err) {
+    console.log('~~ getPostBySlug ~~')
+    console.log(err)
+  }
 }
 
 export const getAllSlugs = async () => {
-    try {
-        const slugs = await client.get({
-            endpoint: 'works',
-            queries: { limit: 10, fields: 'title,slug' },
-        })
-        return slugs.contents
-    } catch (err) {
-        console.log('~~ getAllSlugs ~~');
-        console.log(err);
-    }
+  try {
+    const slugs = await client.get({
+      endpoint: 'works',
+      queries: { limit: 10, fields: 'title,slug' }
+    })
+    return slugs.contents
+  } catch (err) {
+    console.log('~~ getAllSlugs ~~')
+    console.log(err)
+  }
 }
 
 export const getAllWorks = async (limit = 10) => {
-    try {
-        const allWorks = await client.get({
-            endpoint: 'works',
-            queries: { limit: limit, fields: 'title,slug,topImage' },
-        })
-        return allWorks.contents
-    } catch (err) {
-        console.log('~~ getAllWorks ~~');
-        console.log(err);
-    }
+  try {
+    const allWorks = await client.get({
+      endpoint: 'works',
+      queries: { limit: limit, fields: 'title,slug,topImage' }
+    })
+    return allWorks.contents
+  } catch (err) {
+    console.log('~~ getAllWorks ~~')
+    console.log(err)
+  }
 }
