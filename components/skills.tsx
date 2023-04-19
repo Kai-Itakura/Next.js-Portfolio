39f515aa90535ff4daaa70163ab4d6840fa7f800
Kai-Itakura <itakai199969@gmail.com>
2023-04-19 22:33:13 +0900

    skillにTypeScriptを追加

--- components/skills.tsx
+++ components/skills.tsx
@@ -9,6 +9,7 @@ const Skills: React.FC = () => {
         <Bar title='Next.js' src='/nextjs.svg' dataPer={70} />
         <Bar title='React' src='/react.svg' dataPer={60} />
         <Bar title='JavaScript' src='/javascript-icon.svg' dataPer={70} />
+        <Bar title='TypeScript' src='/typescript-icon.svg' dataPer={60} />
         <Bar title='Sass' src='/sass-icon.svg' dataPer={70} />
         <Bar title='Illustrator' src='/illustrator-icon.svg' dataPer={60} />
         <Bar title='Photoshop' src='/photoshop-icon.svg' dataPer={60} />
